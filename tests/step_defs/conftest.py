c49069fb47c519a267293e4393f106a219594a56
Anna_Poimenova <ann.poimenova@gmail.com>
2020-11-17 14:38:34 +0200

    first commit

---
+++ tests/step_defs/conftest.py
@@ -0,0 +1,8 @@
+# import pytest
+# from selenium import webdriver
+#
+# @pytest.fixture(scope="session")
+# def browser():
+#     driver = webdriver.Chrome(executable_path="C:/Users/Anna_Poimenova/PycharmProjects/pythonProject/chromedriver_win32/chromedriver.exe")
+#     yield driver
+#     driver.quit()
